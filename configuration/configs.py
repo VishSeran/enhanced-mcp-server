46d513cdd4aada7ce5616bcb0cb0a53461347e6e
VishSeran <vishwa.sovis@gmail.com>
2026-07-30 13:07:28 +0530

    logger updated

---
+++ configuration/configs.py
@@ -0,0 +1,10 @@
+MODEL_NAME = "llama-3.3-70b-versatile"
+PORT = 9000
+
+
+def get_relative_path(file_path:str)->str:
+    
+    try:
+        
+        
+
---
+++ configuration/logger.py
@@ -0,0 +1,25 @@
+import logging
+
+
+
+def get_logger(name:str):
+    
+    try:
+        
+        logging.basicConfig(
+            level= logging.INFO,
+            format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        
+        name = name.join("-logger")
+        logger = logging.getLogger(name)
+        
+        return logger
+    
+    except ValueError as e:
+        print(f"Value error: {e}")
+        raise
+    
+    except Exception as e:
+        print(f"Error in {name}: {e}")
+        raise
