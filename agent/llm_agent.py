d22d50438ccf7418148ee24dd1a15ed10cd64a72
VishSeran <vishwa.sovis@gmail.com>
2026-07-31 12:51:57 +0530

    llm agent-init fn updated

--- agent/llm_agent.py
+++ agent/llm_agent.py
@@ -4,6 +4,8 @@ import dotenv
 
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+
 
 from configuration.configs import MODEL_NAME
 from configuration.logger import get_logger
@@ -20,21 +22,44 @@ class LLMAgent:
         try:
             GROQ_API = os.getenv("groq_api")
             
+            if not model_name:
+                raise ValueError("Model name is empty")
+            
+            if not GROQ_API:
+                raise ValueError("GROQ_API_KEY is missing from environment")
+            
+            
+            if not tools:
+                raise ValueError("tools list is missing or empty")
+            
+            checkpointer = InMemorySaver()
+            
+            self.config = {
+                "configurable": {
+                    "thread_id": "conversational_id"
+                }
+            }
+            
             self.llm = ChatGroq(
                 api_key=GROQ_API,
                 model=model_name,
                 temperature=0.5
             )
             
+            logger.info(f"{model_name} model initiated")
+            
             self.llm_agent = create_agent(
                 model=self.llm,
                 tools=tools,
+                checkpointer=checkpointer,
                 system_prompt="""
                             You are a useful AI agent.
                             You have access to the tools that provided.
                             Use the relevant tools if needed when answering the user questions.
                 """
             )
+            
+            logger.info("LLM Agent initiated")
             
         except ValueError as e:
             logger.error(f"Value error: {e}")
@@ -51,6 +76,8 @@ class LLMAgent:
             
             if not query:
                 raise ValueError("Query is missing")
+            
+            response = se.llm_agent
             
             
             
