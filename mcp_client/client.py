bbf7ce7d6c5b1dc4de9b3c89f4ed4ae99eec1d0b
VishSeran <vishwa.sovis@gmail.com>
2026-07-31 13:02:16 +0530

    mcp client - init fn updated

--- mcp_client/client.py
+++ mcp_client/client.py
@@ -1,4 +1,6 @@
+from contextlib import AsyncExitStack
 
+from agent.llm_agent import LLMAgent
 from configuration.logger import get_logger
 
 
@@ -6,5 +8,32 @@ logger = get_logger("mcp-client")
 
 class MCPClient:
     
-    def __init__(self):
-        pass
+    def __init__(self, tools):
+        
+        
+        try:
+            
+            self.agent = LLMAgent(tools)
+            # AsyncExitStack for managing async context managers
+            self.exit_stack = AsyncExitStack()
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+            
+        except Exception as e:
+            logger.error(f"Error in mcp client initialization: {e}")
+            raise
+        
+    async def connect_to_mcp_server(self, server_script:str):
+        
+        
+        try:
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+            
+        except Exception as e:
+            logger.error(f"Error in llm agent initialization: {e}")
+            raise
